595da9f69da066cf076411438e00fc4da0a5d837
Eren Yener <eren.yener@trendyol.com>
2017-02-28 15:05:17 +0300

    init tests added

--- specs/specs.syncroll.js
+++ specs/specs.syncroll.js
@@ -1,6 +1,52 @@
 
+QUnit.test( "jquery should included", function( assert ) {
+    var _$ = $;
+    assert.ok( !!_$, "jquery included" );
 
-
-QUnit.test( "first test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
 });
+
+QUnit.test( "synroll should be appended to jquery", function( assert ) {
+    var fn = $.fn;
+    assert.ok( !!fn.syncroll, "syncroll appended to jquery" );
+});
+
+QUnit.test( "type of synroll should be function", function( assert ) {
+    var syncroll = $.fn.syncroll;
+    assert.ok( 'function' === typeof syncroll, "syncroll appended to jquery" );
+});
+
+// QUnit.test( "type of synroll should be function", function( assert ) {
+//   assert.expect(true);
+//
+//   assert.ok( true == "1", "Passed!" );
+//   console.log(leftPanel);
+//   console.log(rightPanel);
+//
+//   // $body.on( "click", function() {
+//   //   assert.ok( true, "body was clicked!" );
+//   // });
+//   //
+//   // $body.trigger( "click" );
+// });
+//
+// QUnit.test( "a test", function( assert ) {
+//   assert.expect( 1 );
+//
+//   var leftPanel =  $('<div/>',{'class':'left-panel'}).width(100).height(100);
+//   var rightPanel =  $('<div/>',{'class':'right-panel'}).width(100).height(100);
+//
+//   console.log(leftPanel);
+//   console.log(rightPanel);
+//
+//   // $body.on( "click", function() {
+//   //   assert.ok( true, "body was clicked!" );
+//   // });
+//   //
+//   // $body.trigger( "click" );
+// });
+//
+//
+//
+// QUnit.test( "first test", function( assert ) {
+//   assert.ok( 1 == "1", "Passed!" );
+// });
