aadd1f3d54894cdc56c61f2a2297d7413e0ff6bd
Eren Yener <eren.yener@trendyol.com>
2017-02-28 13:33:01 +0300

    first test added

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 const gulp = require('gulp');
 const qunit = require('gulp-qunit');
 
-gulp.task('default', () =>
-
 gulp.task('test', function() {
     return gulp.src('./specs/test-runner.html')
         .pipe(qunit());
---
+++ specs/specs.syncroll.js
@@ -0,0 +1,6 @@
+
+
+
+QUnit.test( "first test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
