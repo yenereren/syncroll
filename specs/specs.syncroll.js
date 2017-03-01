07f77170e7817e584c58738d4564ff2f6d58f7fa
Eren Yener <eren.yener@trendyol.com>
2017-03-01 18:10:46 +0300

    tes

--- specs/specs.syncroll.js
+++ specs/specs.syncroll.js
@@ -2,52 +2,76 @@ var boxWidth = 100;
 var bixBoxHeight = 1500;
 var smallBoxHeight = 150;
 
-QUnit.test( "jquery should included", function( assert ) {
+QUnit.test("jquery should included", function(assert) {
     assert.expect(1);
 
     var _$ = $;
-    assert.ok( !!_$, "jquery included" );
+    assert.ok(!!_$, "jquery included");
 });
 
-QUnit.test( "synroll should be appended to jquery", function( assert ) {
+QUnit.test("synroll should be appended to jquery", function(assert) {
     assert.expect(1);
 
     var fn = $.fn;
-    assert.ok( !!fn.syncroll, "syncroll appended to jquery" );
+    assert.ok(!!fn.syncroll, "syncroll appended to jquery");
 });
 
-QUnit.test( "type of synroll should be function", function( assert ) {
+QUnit.test("type of synroll should be function", function(assert) {
     assert.expect(1);
 
     var syncroll = $.fn.syncroll;
-    assert.ok( 'function' === typeof syncroll, "syncroll appended to jquery" );
+    assert.ok('function' === typeof syncroll, "syncroll appended to jquery");
 });
 
-QUnit.test( "bix box should not be synced-scroll to small box", function( assert ) {
+QUnit.test("bix box should not be synced-scroll to small box", function(assert) {
     assert.expect(1);
 
-    var bigBox = $('<div/>',{'id':'bigBox'}).width(boxWidth).height(bixBoxHeight);
-    var smallBox = $('<div/>',{'id':'smallBox'}).width(boxWidth).height(smallBoxHeight);
+    var bigBox = $('<div/>', {
+        'id': 'bigBox'
+    }).width(boxWidth).height(bixBoxHeight);
+    var smallBox = $('<div/>', {
+        'id': 'smallBox'
+    }).width(boxWidth).height(smallBoxHeight);
 
     var isSynced = bigBox.syncroll(smallBox);
-    assert.notOk(isSynced, "syncroll should return false if big box synced to small box" );
+    assert.notOk(isSynced, "syncroll should return false if big box synced to small box");
 
 });
 
-QUnit.test( "equals boxes should not be synced-scroll", function( assert ) {
-    var bigBox = $('<div/>',{'id':'bigBox'}).width(boxWidth).height(bixBoxHeight);
-    var smallBox = $('<div/>',{'id':'smallBox'}).width(boxWidth).height(bixBoxHeight);
+QUnit.test("equals boxes should not be synced-scroll", function(assert) {
+    var bigBox = $('<div/>', {
+        'id': 'bigBox'
+    }).width(boxWidth).height(bixBoxHeight);
+    var smallBox = $('<div/>', {
+        'id': 'smallBox'
+    }).width(boxWidth).height(bixBoxHeight);
 
     var isSynced = bigBox.syncroll(smallBox);
-    assert.notOk(isSynced, "syncroll should return false if big box synced to small box" );
+    assert.notOk(isSynced, "syncroll should return false if big box synced to small box");
 });
 
-QUnit.test( "small box should be synced-scroll to bix box's bottom", function( assert ) {
-    var bigBox = $('<div/>',{'id':'bigBox'}).width(boxWidth).height(bixBoxHeight);
-    var smallBox = $('<div/>',{'id':'smallBox'}).width(boxWidth).height(smallBoxHeight);
+QUnit.test("small box should be synced-scroll to bix box's bottom", function(assert) {
+    var bigBox = $('<div/>', {
+        'id': 'bigBox'
+    }).width(boxWidth).height(bixBoxHeight);
+    var smallBox = $('<div/>', {
+        'id': 'smallBox'
+    }).width(boxWidth).height(smallBoxHeight);
 
     var isSynced = smallBox.syncroll(bigBox);
-    assert.ok(isSynced, "syncroll should return false if big box synced to small box" );
+    assert.ok(isSynced, "syncroll should return false if big box synced to small box");
+});
+
+QUnit.test("small box should be synced-scroll to bix box's bottom 2", function(assert) {
+    var bigBox = $('<div/>', {
+        'id': 'bigBox'
+    }).width(boxWidth).height(bixBoxHeight);
+    var smallBox = $('<div/>', {
+        'id': 'smallBox'
+    }).width(boxWidth).height(smallBoxHeight);
+
+    var isSynced = smallBox.syncroll(bigBox);
+    assert.ok(isSynced, "syncroll should return false if big box synced to small box");
 });
 
 // QUnit.test( "type of synroll should be function", function( assert ) {
