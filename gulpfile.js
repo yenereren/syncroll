04136990e937977eeafb195b0165521bd41db61a
Eren Yener <eren.yener@trendyol.com>
2017-03-03 10:21:50 +0300

    scroll bind imp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp');
 const qunit = require('gulp-qunit');
+const coveralls = require('gulp-coveralls');
+
+gulp.task('coveralls', function() {
+    return gulp.src('./specs/test-runner.html')
+        .pipe(coveralls());
+});
 
 gulp.task('test', function() {
     return gulp.src('./specs/test-runner.html')
         .pipe(qunit());
 });
 
-gulp.task('default', ['test']);
+gulp.task('default', ['test', 'coveralls']);
--- syncroll/syncroll.js
+++ syncroll/syncroll.js
@@ -35,18 +35,29 @@
         this.$window = $(window);
         this.$element = $(element);
         this.$syncTo = $(syncTo);
-        this.$currentScrollPosition = 0;
+
+        this.elemenDistanceTop = this.$element.offset().top;
+        this.elementHeight = this.$element.height();
+        this.syncToElementOffsetBottom = this.$syncTo.offset().top + this.$syncTo.height();
+
+        this.currentScrollPosition = 0;
         this.options = $.extend({}, Syncroll.Defaults, options);
+        this.constants = Syncroll.Constants;
         this.init();
 
     }
 
     Syncroll.Defaults = {
-        debugMode: false
+        debugMode: false,
+        paddingTop: 10
     };
 
     Syncroll.Constants = {
-
+        sections: {
+            top: 'top',
+            scrollableArea: 'scrollableArea',
+            bottom: 'bottom',
+        }
     };
 
     Syncroll.prototype.init = function() {
@@ -63,9 +74,49 @@
         var self = this;
 
         $(document).on('scroll', function() {
-            var scrollTop = $(this).scrollTop();
-            console.log(scrollTop);
+            console.log(" ");
+            console.log("---------------------on scroll");
+            self.currentScrollPosition = $(this).scrollTop();
+            self.sync();
+            console.log("---------------------sync finished");
+            console.log(" ");
         });
+    };
+
+    Syncroll.prototype.sync = function() {
+        var self = this;
+
+        var section = self.getSection();
+        console.log("section -> " + section);
+
+        if (section === self.constants.sections.scrollableArea) {
+            self.$element.css('position', 'fixed');
+            self.$element.css('top', self.options.paddingTop);
+        } else if (section === self.constants.sections.bottom) {
+            var topPosition = self.syncToElementOffsetBottom - self.$element.height() - $('#scrollableContainer').offset().top;
+            self.$element.css('position', 'absolute');
+            self.$element.css('top', topPosition);
+        } else {
+            self.$element.css('position', 'relative');
+            self.$element.css('top', 0);
+        }
+    };
+
+    Syncroll.prototype.getSection = function() {
+        var self = this;
+        var section = self.constants.sections.top;
+
+        var bottom = self.$element.offset().top + self.elementHeight;
+        var leftToTop = self.elemenDistanceTop - self.currentScrollPosition;
+        console.log("bottom -> " + bottom);
+        console.log("leftToTop -> " + leftToTop);
+        if (bottom >= self.syncToElementOffsetBottom) {
+            section = self.constants.sections.bottom;
+        } else if (Syncroll.Defaults.paddingTop >= leftToTop) {
+            section = self.constants.sections.scrollableArea;
+        }
+
+        return section;
     };
 
     Syncroll.prototype.log = function(message) {
